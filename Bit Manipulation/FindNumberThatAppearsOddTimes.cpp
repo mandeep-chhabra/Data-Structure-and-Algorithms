fe7ab1505d5e76054211701d61ff2d6bf70e0bad
mandeep <mandeepsinghchhabra2022@vitbhopal.ac.in>
2025-06-12 23:34:45 +0530

    Daily Challenge 12.06.2025

---
+++ Bit Manipulation/FindNumberThatAppearsOddTimes.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findOddOccurence(int arr[], int n)
+{
+    int res = 0;
+    for (int i = 0; i < n; i++)
+    {
+        res = res ^ arr[i];
+    }
+    return res;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << findOddOccurence(arr, n);
+    return 0;
+}
