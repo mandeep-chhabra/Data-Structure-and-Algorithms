6963152234a9f77eb97dcf7c82363b5edfebef81
mandeep <mandeepsinghchhabra2022@vitbhopal.ac.in>
2025-08-15 23:40:22 +0530

    Daily Challenge 15.08.2025

---
+++ String/FactorialOfLargeNumbers.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void multiply(vector<int> &res, int x)
+{
+    int carry = 0;
+    for (int i = 0; i < res.size(); i++)
+    {
+        int prod = res[i] * x + carry;
+        res[i] = prod % 10;
+        carry = prod / 10;
+    }
+    while (carry)
+    {
+        res.push_back(carry % 10);
+        carry /= 10;
+    }
+}
+
+vector<int> factorial(int n)
+{
+    vector<int> res;
+    res.push_back(1);
+    for (int x = 2; x <= n; x++)
+    {
+        multiply(res, x);
+    }
+    reverse(res.begin(), res.end());
+    return res;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> result = factorial(n);
+    cout << "Factorial of " << n << " is: ";
+    for (int digit : result)
+    {
+        cout << digit;
+    }
+    cout << endl;
+    return 0;
+}
